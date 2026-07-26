ffbe149c09ffbb11c14ae15d1f86e7e52665964d
priyadip <saupriyadip571@gmail.com>
2026-07-27 00:42:49 +0530

    leetvault: sync 8 new accepted submissions

---
+++ Problems/maximum-product-of-three-numbers/history/submission_2082121598.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        fm = sm = tm = float('-inf')
+        fmi = smi = float('inf')
+        for x in nums:
+            if x>= fm:
+                tm = sm
+                sm = fm
+                fm = x
+            elif x>=sm:
+                tm = sm
+                sm = x
+            elif x > tm:
+                tm = x
+
+            if x <= fmi:
+                smi = fmi
+                fmi = x
+            elif x < smi:
+                smi = x
+        return fm*sm*tm if (fm*sm*tm) > (fmi*smi*fm) else fmi*smi*fm
+            
+                
+        
