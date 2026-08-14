e9cc442c1234af4a63dda42fb8f52ef79d494b22
Priyadip Sau <saupriyadip571@gmail.com>
2026-08-14 20:00:57 +0530

    leetvault: sync 4 new accepted submissions

--- Problems/find-the-encrypted-string/history/submission_2106690454.py
+++ Problems/find-the-encrypted-string/history/submission_2106690454.py
@@ -1,6 +1,8 @@
 class Solution:
     def getEncryptedString(self, s: str, k: int) -> str:
-        return s[k%len(s):]+s[:k%len(s)]
+        n = len(s)
+        k %= n
+        return s[k:]+s[:k]
 
 
 
